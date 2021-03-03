03b211b610af8cfaf3f3542f92db3f1e06db4e21
anirudhbelwadi <belwadi.anirudh18@siesgst.ac.in>
2021-03-03 12:06:24 +0400

    added dropdown and titles

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+function showAboutDropdown() {
+    document.getElementById("nav_about_dropdown").style.visibility = "visible";
+}
+const $dropdown = $('.nav_about_dropdown');
+$(document).mouseup(e => {
+    if (!$dropdown.is(e.target) // if the target of the click isn't the container...
+        && $dropdown.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        $dropdown.removeClass('nav_about_dropdown_visible');
+    }
+});
+
+$('.nav_about').on('click', () => {
+    $dropdown.toggleClass('nav_about_dropdown_visible');
+});
